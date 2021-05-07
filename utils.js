bb09f856d58e4bfe3cb469c27bca7dd423e297b4
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-05-07 14:43:53 -0700

    refactored function

--- utils.js
+++ utils.js
@@ -1,24 +1,21 @@
 export function rockPaperScissor(userThrow, computerChoice) {
     if (userThrow === 'rock' && computerChoice === 'rock') {
         return 'draw';
-    } else if (userThrow === 'rock' && computerChoice === 'paper') {
-        return 'lose';
-    } else if (userThrow === 'rock' && computerChoice === 'scissor') {
-        return 'win';
-    } else if (userThrow === 'scissor' && computerChoice === 'scissor') {
-        return 'draw';
-    } else if (userThrow === 'scissor' && computerChoice === 'rock') {
-        return 'lose';
-    } else if (userThrow === 'scissor' && computerChoice === 'paper') {
-        return 'win';
     } else if (userThrow === 'paper' && computerChoice === 'paper') {
         return 'draw';
+    } else if (userThrow === 'scissor' && computerChoice === 'scissor') {
+        return 'draw';
+    } else if (userThrow === 'rock' && computerChoice === 'scissor') {
+        return 'win';
+    } else if (userThrow === 'scissor' && computerChoice === 'paper') {
+        return 'win';
     } else if (userThrow === 'paper' && computerChoice === 'rock') {
         return 'win';
-    } else if (userThrow === 'paper' && computerChoice === 'scissor') {
+    } else {
         return 'lose';
     } 
-}     
+}
+
 export function getRandomThrow() {
     const randomNumber = Math.ceil(Math.random() * 3);
 
